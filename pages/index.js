06727323d2f3d73bef663528f81c61ea6bf3d193
Iury Rodrigues <144119955+iuryss@users.noreply.github.com>
2025-12-15 20:20:32 +0000

    frase especial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Olha ai meu mo, um site com a declaração pra voce ceça linda</h1>;
+  return (
+    <h1>
+      Te amo te amo te amo, mas vc nao me da bola, eu vou me afogar num copinho
+      de coca cola
+    </h1>
+  );
 }
 
 export default Home;
