32be481b609e40a8340f5015c1e5aa6a496a8a2b
Iury <iuryrbs@edu.unifor.br>
2026-01-06 11:53:48 -0300

    alterações

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,38 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js";
+
+export default async function status(request, response) {
+  const dbCLient = await database.getNewClient();
+  const defaultMigrationOptions = {
+    dbClient: dbCLient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbCLient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+
+    await dbCLient.end();
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405).end();
+}
