4011e626f6c98b18b9172e9dd9a8d723b3c14db1
Iury <iuryrbs@edu.unifor.br>
2026-01-20 11:06:12 -0300

    makes `npm test` more robust with `async-retry` and `orchestrator.js`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Te amo te amo te amo, mas vc nao me da bola, eu vou me afogar num copinho
-      de coca cola
-    </h1>
-  );
+  return <h1>Cuida estudar</h1>;
 }
 
 export default Home;
